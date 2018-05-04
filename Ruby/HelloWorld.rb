87ee8a082e23d9f66df0c59f87e758529d6ddc24
Fabio Luiz Régis <fabioreg@inmetrics.com.br>
2018-05-04 14:01:14 -0300

    Subindo arquivo HelloWorld.rb

---
+++ Ruby/HelloWorld.rb
@@ -0,0 +1,23 @@
+# Isto é um comentário
+# Para adicionar um comentário, é só colocar o símbolo '#' na frente da frase.
+
+# O hello world em Ruby é relativamente simples. Posso imprimir na tela de duas formas inicialmente.
+
+
+# Utilizando puts
+# puts, imprime a informação na tela e pula uma linha
+puts 'Olá, Mundo'
+puts 'Pulei uma linha'
+
+
+# Utilizando print
+# print, imprime a informação na tela e continua na mesma linha
+print 'Olá,  mundo!'
+print 'Continuo na mesma linha'
+
+
+# Para rodar este programa em Ruby é só ir na linha de comando e com o Ruby devidamente instalado,
+# digitar ruby <nome_do_arquivo.rb>
+# no caso:
+#
+# > ruby HelloWorld.rb
