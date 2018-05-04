ee8d781d5fe4148705cb693a29a41b0423ab8e8f
Fabio Luiz Régis <fabioreg@inmetrics.com.br>
2018-05-04 15:27:17 -0300

    Subindo arquivo de operações matemáticas básicas OpecoesMatematicas.rb

---
+++ Ruby/OpecoesMatematicas.rb
@@ -0,0 +1,23 @@
+# Como realizar operações matemáticas!?
+# É isto que iremos ver agora, operações matemáticas básicas.
+# Utilizaremos os seguintes operadores:
+#     + para adição
+#     - para subtração
+#     * para multiplicação
+#     / para divisão
+#     % para o resto  da divisão inteira
+
+puts "Quanto é 20 + 3?"
+puts 20 + 3
+
+puts "\nQuanto é 20 - 3?"
+puts 20 - 3
+
+puts "\nQuanto é 20 * 3?"
+puts 20 * 3
+
+puts "\nQuanto é 20 / 3?"
+puts 20 / 3
+
+puts "\nQuanto é o resto da divisão inteira de 20 por 3?"
+puts 20 % 3
