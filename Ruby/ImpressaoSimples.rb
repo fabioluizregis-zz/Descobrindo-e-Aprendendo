0d61a084b3635a07a64f9f0f2dc34e85ed53a9d0
Fabio Luiz Régis <fabioreg@inmetrics.com.br>
2018-05-04 15:10:25 -0300

    Adicionando novo arquivo ImpressaoSimples.rb para outras formas de impressão

---
+++ Ruby/ImpressaoSimples.rb
@@ -0,0 +1,21 @@
+# Aqui, exemplificarei outras formas de impressão
+# Comando 	O que isto faz?
+# \\ 	      Insere uma barra
+# \' 	      Insere aspas simples
+# \" 	      Insere aspas duplas
+# \a 	      Toca sineta da bios
+# \b 	      Faz a função de backspace
+# \n 	      Pular linha
+# \r 	      Carriage Return (Volta a posição de escrita para o começo)
+# \t 	      (TAB) - Insere Tab Horizontal
+
+
+puts "\n\nPulando duas linhas antes de começar a imprimir"
+puts "Vamos aos exemplos:"
+puts "\\Utilizando \\barras"
+puts "Utilizando \'aspas simples\'"
+puts "Utilizando \"aspas duplas\""
+puts "Tocando o sino da bios.. \a (precisa estar habilitado na bios)"
+puts "Backspacee\b - Tinha 2 es em Backspace.. agora apareceu correto =)"
+puts " Ja foi manolo? \rVoltou - Volta a escrita para o início da linha"
+puts "HorizontalTab\tHorizontalTab\tHorizontalTab"
